462f0bef1591098844127fc7ae4c77bcb177b662
Aldwin-b <78974528+Aldwin-b@users.noreply.github.com>
2023-02-19 19:18:40 +0100

    Header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import "./site_styles.css";
+
+const Navbar = () => {
+  return (
+    <nav className="background">
+      <div className="container">
+        <p>ozeiuhhdhhfhizue</p>
+        <p>igfnjhnvnonnern</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
