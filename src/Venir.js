60d437c20433e07d074a2a65330213e80981d9a3
Aldwin-b <78974528+Aldwin-b@users.noreply.github.com>
2024-06-24 18:20:15 +0200

    New maps free of charge, no more google api

--- src/Venir.js
+++ src/Venir.js
@@ -1,5 +1,5 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import config from "./config/config"; // Chemin vers votre fichier config.js
+//import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+//import config from "./config/config"; // Chemin vers votre fichier config.js
 
 const screenWidth = window.innerWidth;
 let containerStyle = {
@@ -20,7 +20,7 @@ if (screenWidth >= 800) {
     height: "40vh",
   };
 }
-
+/*
 const center = {
   lat: 45.271453,
   lng: -0.395978,
@@ -30,7 +30,7 @@ const markerPosition = {
   lat: 45.271846,
   lng: -0.398084,
 };
-
+*/
 const Venir = () => {
   console.log();
   return (
@@ -38,15 +38,14 @@ const Venir = () => {
       <div className="venir_title title mobile_page_title">Comment venir</div>
       <div className="venir_infos">
         <div className="venir_carte">
-          <LoadScript googleMapsApiKey={config.mapsAPIkey}>
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              center={center}
-              zoom={15}
-            >
-              <Marker position={markerPosition} />
-            </GoogleMap>
-          </LoadScript>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d306.7224946019668!2d-0.39822888112799815!3d45.27166116760003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480039f1682c34e7%3A0xefb0ca49946030b8!2sFestival%20Dr%C3%B4les%20de%20M%C3%B4mes!5e0!3m2!1sfr!2sfr!4v1719245438444!5m2!1sfr!2sfr"
+            allowfullscreen=""
+            style={containerStyle}
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            title="Carte du festival Drôles de Mômes"
+          ></iframe>
         </div>
         <div className="venir_texts">
           <div className="text_W venir_subtitle">
