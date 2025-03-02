15a7b3cdee565aa02c150162fca4fe32784957bb
Aldwin Baïetto <niwdla1234@yahoo.fr>
2025-03-02 16:01:23 +0100

    update main page for impro match 25

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import main_picture from "./images/main_picture.png";
+import main_picture from "./images/matchs/25/match_2025.jpg";
 import { Link } from "react-router-dom";
 
 const Main = () => {
@@ -8,33 +8,28 @@ const Main = () => {
         <img src={main_picture} className="main_picture" alt="" />
       </div>
       <div className="main_logo_text">
+        <div className=" title main_title">Soirée match d'impro</div>
         <div id="text_prog" className="main_text_prog_container">
           <span className="text_W">
-            Les temps sont durs pour la culture et les petits festivals. Mais
-            depuis 2005, le festival Drôles de Mômes et sa drôle d’équipe de
-            bénévoles se battent pour faire vivre les arts vivants, le temps
-            d’un week-end, en milieu rural. Et 2024 ne les arrêtera pas ! Cette
-            équipe est multigénérationnelle, multicompétente, pleine d’idées,
-            d’imagination et de rêves. Alors on s’accroche, on donne de notre
-            temps et de notre énergie pour préparer le festival et lui donner
-            vie, constuire en une semaine à peine un village éphémère qui
-            célèbre le théâtre, la musique et le partage. La 18ème édition sera
-            comme toujours joyeuse, éclectique et responsable. Vous pourrez vous
-            restaurez et vous hydrater localement, vous reposer sous la tente
-            saharienne, admirer les expositions, retomber en enfance avec les
-            dizaines de jeux en bois qui parsèmeront notre site coloré... Et
-            bien sûr profiter d’une programmation riche et variée : neuf
-            spectacles et six concerts en deux jours ! Cerise sur le chapiteau,
-            la Diva Mobile – la plus petite boîte de nuit du monde – sera de
-            retour pour des sessions dansantes enflammées avec les plus
-            grand·e·s DJ de la région.
+            <b> Le samedi 5 avril dès 20h</b>, assistez à la toute première
+            cérémonie des Golden Mômes, une soirée de remise de prix célébrant
+            les vingt dernières années de l'association La Machine à Bulles et
+            du festival Drôles de Mômes.
+            <br />
+            <br />
+            L'entrée sera à <b>prix libre</b> et tous les bénéfices de la soirée
+            serviront à organiser la prochaine du festival Drôles de Mômes{" "}
+            <b>les 11 et 12 juillet 2025 !</b> <br />
+            <br />
+            Le match d'improvisation sera suivi d'un concert du groupe BoKaL et
+            d'un DJ set surprise.
           </span>
         </div>
 
         <div id="prog" className="button_container">
           <button className="button">
-            <Link className="link" to="/programmation">
-              PROGRAMMATION 2024
+            <Link className="link" to="/matchsimpro?match=1">
+              EN SAVOIR PLUS{" "}
             </Link>
           </button>
         </div>
